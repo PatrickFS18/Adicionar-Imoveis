c9d9017dd025f9537b38f2870a7037c745e2e3c8
l <61829586+Whesley696@users.noreply.github.com>
2023-06-04 19:21:14 -0300

    Add files via upload

--- resources/views/pages/imobiliaria.blade.php
+++ resources/views/pages/imobiliaria.blade.php
@@ -8,90 +8,56 @@
     <link href="css/style.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <style>
-body {
-  background-color: #222;
-  color: #fff;
-  font-family: Arial, sans-serif;
-  margin: 0;
-  padding: 0;
-}
+        body {
+            background-image: linear-gradient(to bottom, #222, #000);
+            color: #fff;
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
 
-.container {
-  max-width: 800px;
-  margin: 0 auto;
-  padding: 20px;
-}
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+        }
 
-h1 {
-  text-align: center;
-  margin-bottom: 30px;
-}
+        h1 {
+            text-align: center;
+            margin-bottom: 30px;
+        }
 
-.table {
-  background-color: #222;
-  color: #fff;
-  border-radius: 5px;
-  margin-bottom: 20px;
-}
+        .table {
+            background-color: #222;
+            color: #fff;
+            border-radius: 5px;
+        }
 
-.table th,
-.table td {
-  padding: 10px;
-  border-bottom: 1px solid #444;
-}
+        .table th,
+        .table td {
+            padding: 10px;
+            border-bottom: 1px solid #444;
+        }
 
-.btn {
-  display: inline-block;
-  padding: 10px 20px;
-  font-size: 16px;
-  font-weight: bold;
-  text-decoration: none;
-  background-color: #007bff;
-  color: #fff;
-  border: none;
-  cursor: pointer;
-}
+        .btn {
+            display: inline-block;
+            padding: 10px 20px;
+            font-size: 16px;
+            font-weight: bold;
+            text-decoration: none;
+            background-color: #007bff;
+            color: #fff;
+            border: none;
+            cursor: pointer;
+        }
 
-.form-group {
-  margin-bottom: 20px;
-}
-
-.form-group label {
-  display: block;
-  margin-bottom: 5px;
-}
-
-.form-group input,
-.form-group select {
-  width: 100%;
-  padding: 8px;
-  font-size: 16px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-}
-
-.form-group input[type="submit"] {
-  margin-left: 40%;
-}
-
-.form-group input[type="text"]:focus,
-.form-group select:focus {
-  outline: none;
-  border-color: #007bff;
-}
-
-.alert {
-  margin: 10px;
-}
-
-.footer {
-  text-align: center;
-  padding: 10px;
-  background-color: #222;
-  color: #fff;
-  margin-top: 2em;
-}
-
+        .footer {
+            text-align: center;
+            padding: 10px;
+            background-color: #222;
+            color: #fff;
+            margin-top: 2em;
+        }
     </style>
 </head>
 
@@ -131,25 +97,234 @@ h1 {
                         <option value="Aluguel">Aluguel</option>
                         <option value="Venda">Venda</option>
                     </select>
-    </div>
+                </div>
             </section>
 
             <div class="form-group">
-                <label for="imobiliaria-description">Descrição</label>
-                <textarea id="imobiliaria-description" name="descricao"></textarea>
-            </div>
-
-            <div class="form-group">
-                <button type="submit" class="btn btn-primary">Adicionar Imóvel</button>
+                <input type="submit" class="btn" style="margin-left: 40%;" value="Inserir">
             </div>
         </form>
 
-        <div class="footer">
-            &copy; 2023 Imobiliárias. Todos os direitos reservados.
+        <h1>Imóveis</h1>
+        <div id="imoveis-container">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Nome da Casa</th>
+                        <th>Preço</th>
+                        <th>Endereço</th>
+                        <th>Tipo</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @if(isset($casas) && count($casas) > 0)
+                        @foreach($casas as $casa)
+                            <tr>
+                                <td>{{ $casa->nome }}</td>
+                                <td>{{ $casa->preco }}</td>
+                                <td>{{ $casa->endereco }}</td>
+                                <td>{{ $casa->venda }}</td>
+                                <td>
+                                    <a href="" class="btn btn-edit">Editar</a>
+                                    <a href="" class="btn btn-delete">Excluir</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    @else
+                        <tr>
+                            <td colspan="5">Nenhum imóvel disponível</td>
+                        </tr>
+                    @endif
+                </tbody>
+            </table>
         </div>
-    </div>
+        <form id="filtro-form" class="form" method="POST" action="{{ route('processForm') }}">
+            @csrf
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-fTq1GfyuazQSPv4tRgB0+NEi1Jb5wZSS50sH3E4e5r7JZ03ejh2OoQtwep4JYK8E" crossorigin="anonymous"></script>
+            <div class="filter-group">
+                <label for="filter-select">Buscar por:</label>
+                <select id="filter-select" name="filtro">
+                    <option value="casa-cara">Casa mais cara</option>
+                    <option value="casa-aluguel">Casa de aluguel</option>
+                    <option value="casa-venda">Casa de venda</option>
+                    <option value="preco-asc">Preço em ordem crescente</option>
+                    <option value="endereco-asc">Endereço em ordem crescente</option>
+                </select>
+                <button type="submit" class="btn">Filtrar</button>
+            </div>
+        </form>
+        <div style="margin-top:1em"></div>
+        @if(isset($casaMaisCara))
+            <h1>Casa mais cara:</h1>
+            <div id="casa-cara">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Nome da Casa</th>
+                            <th>Preço</th>
+                            <th>Endereço</th>
+                            <th>Tipo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{{ $casaMaisCara->nome }}</td>
+                            <td>{{ $casaMaisCara->preco }}</td>
+                            <td>{{ $casaMaisCara->endereco }}</td>
+                            <td>{{ $casaMaisCara->venda }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        @endif
+
+        @if(isset($Alugueis))
+            <h1>Casas de aluguel:</h1>
+            <div id="casa-aluguel">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Nome da Casa</th>
+                            <th>Preço</th>
+                            <th>Endereço</th>
+                            <th>Tipo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($Alugueis as $casa)
+                            <tr>
+                                <td>{{ $casa->nome }}</td>
+                                <td>{{ $casa->preco }}</td>
+                                <td>{{ $casa->endereco }}</td>
+                                <td>{{ $casa->venda }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        @endif
+
+        @if(isset($Vendas))
+            <h1>Casas de venda:</h1>
+            <div id="casa-venda">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Nome da Casa</th>
+                            <th>Preço</th>
+                            <th>Endereço</th>
+                            <th>Tipo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($Vendas as $casa)
+                            <tr>
+                                <td>{{ $casa->nome }}</td>
+                                <td>{{ $casa->preco }}</td>
+                                <td>{{ $casa->endereco }}</td>
+                                <td>{{ $casa->venda }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        @endif
+
+        @if(isset($PrecoCrescente))
+            <h1>Preço em ordem crescente:</h1>
+            <div id="preco-asc">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Nome da Casa</th>
+                            <th>Preço</th>
+                            <th>Endereço</th>
+                            <th>Tipo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($PrecoCrescente as $casa)
+                            <tr>
+                                <td>{{ $casa->nome }}</td>
+                                <td>{{ $casa->preco }}</td>
+                                <td>{{ $casa->endereco }}</td>
+                                <td>{{ $casa->venda }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        @endif
+
+        @if(isset($EnderecoCrescente))
+            <h1>Endereco Crescente:</h1>
+            <div id="preco-desc">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Nome da Casa</th>
+                            <th>Preço</th>
+                            <th>Endereço</th>
+                            <th>Tipo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($EnderecoCrescente as $casa)
+                            <tr>
+                                <td>{{ $casa->nome }}</td>
+                                <td>{{ $casa->preco }}</td>
+                                <td>{{ $casa->endereco }}</td>
+                                <td>{{ $casa->venda }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        @endif
+
+        <form id="filtro-form" class="form" method="POST" action="{{ route('search') }}">
+            @csrf
+
+            <div class="filter-group">
+                <label for="filter-input">Procurar:</label>
+                <input type="text" id="filter-input" name="Search" placeholder="Digite um trecho do endereço">
+                <input type="submit" class="btn" value="Pesquisar">
+            </div>
+        </form>
+        @if(isset($CasasPesquisadas))
+    <h1>Resultados da pesquisa:</h1>
+    <div id="resultados-pesquisa">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Nome da Casa</th>
+                    <th>Preço</th>
+                    <th>Endereço</th>
+                    <th>Tipo</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($CasasPesquisadas as $casa)
+                    <tr>
+                        <td>{{ $casa->nome }}</td>
+                        <td>{{ $casa->preco }}</td>
+                        <td>{{ $casa->endereco }}</td>
+                        <td>{{ $casa->venda }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endif
+@if(isset($mensagem))
+    <p>{{ $mensagem }}</p>
+@endif
+        <div class="footer">
+        &copy; 2023 Imobiliárias. Todos os direitos reservados.        </div>
+    </div>
 </body>
 
 </html>
+
+       
